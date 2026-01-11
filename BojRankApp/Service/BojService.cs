c93306c810c94b8ad1ddb870f776daf402d1855f
HEK <ghddmsrl1@naver.com>
2026-01-12 02:16:43 +0900

    feat: API 호출 및 비즈니스 로직을 담당할 BojService 클래스 선언 - API 호출에 필요한 HttpClient 객체를 가짐 - 필요 API의 Prefix 선언

---
+++ BojRankApp/Service/BojService.cs
@@ -0,0 +1,25 @@
+﻿using BojRankApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Documents;
+
+namespace BojRankApp.Service
+{
+    internal class BojService
+    {
+
+        private readonly HttpClient _client = new HttpClient();
+        private const string SolvedProblemApiPrefix = "https://solved.ac/api/v3/search/problem";
+        private const string UserApiPrefix = "https://solved.ac/api/v3/user/show";
+        
+    }
+}
