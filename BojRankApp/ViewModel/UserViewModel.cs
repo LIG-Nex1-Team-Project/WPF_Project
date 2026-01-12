589f30b06fdeb0d9a819c050d5f7794ae314d059
whodini5623 <whodini5623@gmail.com>
2026-01-12 16:16:03 +0900

    ResetUser added

--- BojRankApp/ViewModel/UserViewModel.cs
+++ BojRankApp/ViewModel/UserViewModel.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using BojRankApp.Model;
+﻿using BojRankApp.Model;
 using BojRankApp.Service;
-using System.Collections.ObjectModel;
-using System.Threading.Tasks;
-using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace BojRankApp.ViewModel
 {
@@ -36,15 +37,35 @@ namespace BojRankApp.ViewModel
         public async Task AddUser(string userId)
         {
             User user = await bojService.LoadUser(userId);
-            // Debug.WriteLine(userId);
             if (user == null)
             {
                 MessageBox.Show("사용자를 찾을 수 없습니다.");
                 return;
             }
-
             Users.Add(user);
         }
 
+        [RelayCommand]
+        public async Task ResetUser()
+        {
+            var currentList = Users.ToList();
+            Users.Clear();
+
+            foreach (var user in currentList)
+            {
+                var updatedUser = await bojService.LoadUser(user.Id);
+                Users.Add(updatedUser);          
+            }
+
+            var sorted = Users.OrderByDescending(u => u.Rating).ToList();
+            Users.Clear();
+
+            foreach (var u in sorted)
+            {
+                Users.Add(u);
+            }
+
+        }
+        
     }
 }
