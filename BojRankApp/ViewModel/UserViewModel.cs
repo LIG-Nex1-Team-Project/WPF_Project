79dc5c3d47f23618e1d710fe9514fccbc501b375
HEK <ghddmsrl1@naver.com>
2026-01-12 13:13:08 +0900

    feat: UserViewModel Class 추가

---
+++ BojRankApp/ViewModel/UserViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using BojRankApp.Model;
+using BojRankApp.Service;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BojRankApp.ViewModel
+{
+    public partial class UserViewModel: ObservableObject
+    {
+
+        private BojService bojService;
+
+        public ObservableCollection<User> Users { get; }
+
+        [ObservableProperty]
+        private string userId = string.Empty;
+
+
+        public UserViewModel()
+        {
+            bojService = new BojService();
+            Users = new ObservableCollection<User>();
+        }
+
+        [RelayCommand]
+        public async Task AddUserCommand(string userId)
+        {
+            if (string.IsNullOrWhiteSpace(UserId))
+                return;
+
+            User user = await bojService.LoadUser(userId);
+            Users.Add(user);
+        }
+
+    }
+}
